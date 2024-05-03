1e366f2ba56c259f588acaf89cf934a7560a9aa7
abishekn1 <abishekraj0405@gmail.com>
2024-05-03 20:15:01 +0530

    third push

---
+++ Datatypes.java
@@ -0,0 +1,113 @@
+public class Datatypes{
+
+	public static void main(String[] args){
+
+
+		byte planetsInSolarSystem = 8; // improve readability
+
+		byte numberOfSeasons = 3; // manoj
+
+		byte iplTeams = 10; // akbar
+
+		byte age = 23; // sam
+
+		byte standardsInSchool = 12; // vignesh
+
+
+
+
+
+		short priceOfTomato = 100; //
+
+		short angleOfCircle = 360;
+
+		short salaryPerDay = 1500;
+
+		short DaysInAYear = 365;
+
+		short peopleInSmallVillage = 2000;
+
+		short watchPrice = 1000;
+
+		short carSpeed = 140;
+
+
+		int populationOfTN = 768768675;
+
+		int rangeRoverPrice = 20000000;
+
+		int goldprice = 60000;
+
+		int salary = 500000;
+
+		int accountBalance = 1000000;
+
+		long aadharNumber = 6372676273627662l;
+
+		long starsInSky = 768968767677687L;
+
+		long distanceToSun = 15000000000000L;
+
+		long accountNumber = 16862376276276l;
+
+		long networthOfSunilChetri = 122000000000l;
+
+
+		float height = 6.21f;
+
+		float callDuration = 1.23f;
+
+		float bankBalance = 17.42f;
+
+
+
+		double valueOfPie = 3.1415926539d;
+
+		double planksConstant = 6.6260d;
+
+		boolean isItRaining = false;
+
+		boolean hadDinner = false;
+
+		boolean isTodayAHoliday = true;
+
+		boolean isSunTooHot = true;
+
+		boolean feelingGreat  = true;
+
+		boolean isTodaysClassInteresting = true;
+
+		boolean likeCars = true;
+
+		boolean isValid = true;
+
+		boolean isQualified = false;
+
+		boolean isSubmited = true;
+
+		boolean isEveryoneListening = true;
+
+		char grade = 'A';
+
+		char bloodGroup = 'B';
+
+		char section = 'C';
+
+		char initial = 'M';
+
+		char gravityContant = 'G';
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+	
+}
---
+++ Example.java
@@ -0,0 +1,43 @@
+public class Example{
+	
+	public static void main(String[] args){
+
+
+
+		int id = 1;
+		int phoneNumber = 7687575;
+		
+
+		int marks = 100;
+
+		int rollNumber = 24;
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+	}
+
+
+
+
+
+}
